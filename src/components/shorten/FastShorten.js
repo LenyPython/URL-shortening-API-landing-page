fdf9146c4cce83c67cb85b4802d59df0443db642
LenyPython <buniadyserv@gmail.com>
2021-08-16 19:28:41 +0200

    added basic getData function to shorten component to recive data from API

--- src/components/shorten/FastShorten.js
+++ src/components/shorten/FastShorten.js
@@ -1,19 +1,46 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './short.css'
 
+
+
 const FastShorten = () => {
+  const [link, setLink] = useState('Shorten a link here...');
+
+  const handleLinkChange = event => setLink(event.target.value)
+  const handleClick = () => {
+    let url = 'https://api.shrtco.de/v2/shorten?url='
+    getData(url)
+  }
+  const getData = async url => {
+    console.log('connecting')
+    try {
+      let response = await fetch(url + link)
+      response = await response.json()
+      let data = {
+        link: link,
+        short_link: response.result.full_short_link
+      }
+      console.log('inside:')
+      console.log(data)
+      return data
+    }
+    catch (err) {
+      console.log(err)
+    }
+  }
 
   return (
     <section id="shortener">
       <textarea
-        id=""
-        name=""
-        cols="30"
-        rows="10"
-        value='Shorten a link here...'
+        id="linkShortener"
+        name="link"
+        onChange={handleLinkChange}
+        value={link}
+        placeholder='Shorten a link here...'
+        required
       >
       </textarea>
-      <button>Shorten It!</button>
+      <button htmlFor="link" onClick={handleClick}>Shorten It!</button>
     </section>
   )
 }
