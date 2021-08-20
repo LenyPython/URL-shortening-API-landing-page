05c7940ca8c6333a89a0a74790c0425559c81142
LenyPython <buniadyserv@gmail.com>
2021-08-20 19:08:55 +0200

    worked on visuals in shorts scss. Created correct \results container\ for showind shortened links. Created .css classes, and js functionality for copy buttons

--- src/components/shorten/FastShorten.js
+++ src/components/shorten/FastShorten.js
@@ -7,22 +7,21 @@ const FastShorten = () => {
   const [links, setLinks] = useState(sessionStorage.links ? JSON.parse(sessionStorage.links) : []);
   const [link, setLink] = useState('Shorten a link here...');
 
-  let prevLinks = links.map((item, idx) => {
-    return <li key={idx}>normal: {item.link}, short:{item.short_link} <button>Click</button></li>
-  })
 
+  const handleCopyShortLink = event => {
+    let prevCopiedButton = document.querySelector('.Copied')
+    if (prevCopiedButton) prevCopiedButton.classList.remove('Copied')
+    event.target.innerText = 'Copied!'
+    event.target.classList.add('Copied')
+  }
 
   const handleLinkChange = event => setLink(event.target.value)
 
-  const handleClick = () => {
+  const handleClick = async () => {
     let url = 'https://api.shrtco.de/v2/shorten?url=' + link
-    getData(url)
-  }
-
-  const getData = async url => {
     try {
       console.log('connecting')
-      let response = await fetch(url + link)
+      let response = await fetch(url)
       console.log('recieved response')
       response = await response.json()
       let data = {
@@ -30,7 +29,7 @@ const FastShorten = () => {
         short_link: response.result.full_short_link
       }
       let currentLinks = sessionStorage.links ? JSON.parse(sessionStorage.links) : []
-      if (currentLinks.length >= 4) currentLinks.pop()
+      if (currentLinks.length >= 3) currentLinks.pop()
       currentLinks.unshift(data)
       sessionStorage.links = JSON.stringify(currentLinks)
       setLinks(currentLinks)
@@ -39,6 +38,13 @@ const FastShorten = () => {
       console.log(err)
     }
   }
+
+  let prevLinks = links.map((item) => {
+    return (<div key={item.link}>
+      <p>{item.link}</p>
+      <div><p>{item.short_link}</p><button onClick={handleCopyShortLink}>Copy</button></div>
+    </div>)
+  })
 
   return (
     <section id="shorten">
@@ -55,9 +61,7 @@ const FastShorten = () => {
         <button htmlFor="link" onClick={handleClick}>Shorten It!</button>
       </div>
       <div id="results">
-        <ul>
-          {links.length > 0 ? prevLinks : 'none'}
-        </ul>
+        {links.length > 0 ? prevLinks : null}
       </div>
     </section>
   )
