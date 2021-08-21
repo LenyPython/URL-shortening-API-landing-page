97b2f802a97dec27b730ac7cd7ebaecb0067048a
LenyPython <buniadyserv@gmail.com>
2021-08-21 20:32:26 +0200

    Created custom Loader svg for fetching api data. Added special animation to it. Created an error info if fetching data would not succed

--- src/components/shorten/results/Results.js
+++ src/components/shorten/results/Results.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {ReactComponent as Loader} from './loader.svg'
 import './results.css'
 
 const Results = ({stateControl}) => {
@@ -19,12 +20,13 @@ const Results = ({stateControl}) => {
 
 
   let prevLinks = links.map((item, idx) => {
+    if (item === 'error') return <p class="error">Error occured</p>
     let shortLink = (<div key={item.link}>
       <p>{item.link}</p>
       <div><p>{item.short_link}</p><button onClick={handleCopyShortLink}>Copy</button></div>
     </div>)
 
-    if (idx === 0) return isLoading ? 'Awaiting response...' : shortLink
+    if (idx === 0) return isLoading ? <Loader key={0} /> : shortLink
     return shortLink
   })
   return (
